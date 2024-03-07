427db4beebd3c3bfd32d40bc5978dc8ce2b618bf
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2024-03-08 00:38:02 +0100

    feat: ui lib

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-
+import React from 'react';
+import { View, Text, Button } from 'react-native-ui-lib';
 
 export default function App() {
-
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View flex paddingH-25 paddingT-120>
+      <Text blue50 text50>React Native UI Lib Example</Text>
+      <Button label="Click Me" onPress={() => {}} />
     </View>
   );
 }
